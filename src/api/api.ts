52357de28c0586240363c87b042d690ed316e989
spandetectea@gmail.com <spandetectea@gmail.com>
2022-10-03 18:06:57 +0300

    added typescript2

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: 'http://api.weatherapi.com/v1',
-    headers: {
-        // 'X-API-KEY': '2db36e01-65c5-4c85-bcc8-42996fbff616', //gmail
-        // 'X-API-KEY': 'b981956399fc444dbdf162056222809', //yandex
-    }
 });
 
 export const weatherApi = {
